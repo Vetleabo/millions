016862b72ba374be0f977d327464a186194bb916
Vetleabo <vetleabo@hotmail.com>
2026-02-24 15:13:16 +0100

    Add project structure with main and test

---
+++ src/main/java/edu/ntnu/idatx2003/vab/millions/Main.java
@@ -0,0 +1,8 @@
+package edu.ntnu.idatx2003.vab.millions;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Millions project is set up");
+  }
+}
+
---
+++ src/test/java/edu/ntnu/idatx2003/vab/millions/SmokeTest.java
@@ -0,0 +1,12 @@
+package edu.ntnu.idatx2003.vab.millions;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SmokeTest {
+
+  @Test
+  void mavenWorks() {
+    assertTrue(true);
+  }
+}
